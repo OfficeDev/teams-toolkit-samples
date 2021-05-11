21e288201b2ddaa24a8b25fabde0ac65b4c5e923
Zhaofeng Xu <zhaofengxu@microsoft.com>
2021-05-11 16:58:42 +0800

    add header

--- share-now/api/utils/query.ts
+++ share-now/api/utils/query.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 import { executeQuery } from "./common";
 
 export async function checkPost(postID, conn, userID = ""): Promise<boolean> {
