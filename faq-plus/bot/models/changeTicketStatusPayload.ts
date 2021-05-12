d35218d6608cfff9b76193cd1baf0e7b766254c3
Chaoyi Yuan <chyuan@microsoft.com>
2021-05-12 16:39:08 +0800

    feat: add tickets provider

--- faq-plus/bot/models/changeTicketStatusPayload.ts
+++ faq-plus/bot/models/changeTicketStatusPayload.ts
@@ -1,1 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
+export class ChangeTicketStatusPayload {
+    public static readonly reopenAction: string = "Reopen";
+    public static readonly closeAction: string = "Close";
+    public static readonly assignToSelfAction: string = "AssignToSelf";
+
+    public ticketId: string;
+    public action: string;
+}
---
+++ faq-plus/bot/models/ticketState.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export enum TicketState {
+    Open = 0,
+    Closed = 1,
+    MaxValue = Closed
+}
