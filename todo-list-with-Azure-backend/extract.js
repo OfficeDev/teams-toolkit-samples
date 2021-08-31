1532bccdefc371fb26a1609ee59da939596807ad
Ivan He <ruhe@microsoft.com>
2021-08-31 13:44:30 +0800

    chore: make extract.js robust, rename teamsfx package

--- todo-list-with-Azure-backend/extract.js
+++ todo-list-with-Azure-backend/extract.js
@@ -1,15 +1,27 @@
 'use strict'
 
-const fs = require('fs')
+const fs = require('fs');
+
+if (process.argv.length < 4) {
+  console.log('Wrong format. Usage: node ./extract.js <group> <key>');
+  process.exit(1);
+}
 
 const group = process.argv[2];
 const key = process.argv[3];
 
-if (!key) {
+if (!group || !key) {
+  console.log('Invalid group or key entered.')
   process.exit(1);
 }
 
-let rawData = fs.readFileSync('.fx/env.default.json')
-let config = JSON.parse(rawData)
+const rawData = fs.readFileSync('.fx/env.default.json');
+const config = JSON.parse(rawData);
 
-console.log(config[group][key])
+const candidate = config[group]?.[key];
+if (!candidate) {
+  console.log(`Cannot find the config by config[${group}][${key}].`);
+  process.exit(1);
+}
+
+console.log(candidate);
