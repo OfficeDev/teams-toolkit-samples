862d94f3b735cc3865a0a919d266508b78022240
Xiaofu Huang <xiaofhua@microsoft.com>
2022-03-31 17:14:47 +0800

    chore(notification): add incoming webhook sample

---
+++ incoming-webhook-notification/incoming-webhook/src/index.ts
@@ -0,0 +1,25 @@
+// Import required packages
+import { IncomingWebhookTarget, NotificationTarget } from "@microsoft/teamsfx";
+import { buildAdaptiveCard } from "./adaptiveCard";
+import * as fs from "fs-extra";
+import * as path from "path";
+
+const url: string = "<webhook-url>";
+const target: NotificationTarget = new IncomingWebhookTarget(new URL(url));
+const testNames = ["default", "columnset", "factset", "list", "mention"];
+
+async function triggerIncomingWebhook<TData, TTemplate>(dataFileName: string, templateFileName: string) {
+    const cardData = await fs.readJson(dataFileName);
+    const cardTemplate = await fs.readJson(templateFileName);
+    await target.sendAdaptiveCard(buildAdaptiveCard(() => {
+        return cardData;
+    }, cardTemplate));
+}
+
+for (const name of testNames) {
+    const dataFileName = path.join(__dirname, `./adaptiveCards/notification-${name}.data.json`);
+    const templateFileName = path.join(__dirname, `./adaptiveCards/notification-${name}.json`);
+    triggerIncomingWebhook(dataFileName, templateFileName)
+    .then(() => console.log(`Send ${name} adaptive card suffessfully.`))
+    .catch(e => console.log(`Failed to send ${name} adaptive card. ${e}`));
+}
