52d3c407fdc4d53352a65a5a2f946596b2b162f6
zhijie <zhihuan@microsoft.com>
2022-05-19 00:32:08 +0800

    chore: update hello world tab with backend

--- hello-world-tab-with-backend/api/getUserProfile/index.ts
+++ hello-world-tab-with-backend/api/getUserProfile/index.ts
@@ -7,11 +7,7 @@
 import "isomorphic-fetch";
 import { Context, HttpRequest } from "@azure/functions";
 import { Client } from "@microsoft/microsoft-graph-client";
-import {
-  createMicrosoftGraphClient,
-  TeamsFx,
-  UserInfo,
-} from "@microsoft/teamsfx";
+import { createMicrosoftGraphClient, TeamsFx, UserInfo } from "@microsoft/teamsfx";
 
 interface Response {
   status: number;
@@ -26,6 +22,7 @@ type TeamsfxContext = { [key: string]: any };
  * Before trigger this function, teamsfx binding would process the SSO token and generate teamsfx configuration.
  *
  * This function initializes the teamsfx SDK with the configuration and calls these APIs:
+ * - TeamsFx().setSsoToken() - Construct teamsfx instance with the received SSO token and initialized configuration.
  * - getUserInfo() - Get the user's information from the received SSO token.
  * - createMicrosoftGraphClient() - Get a graph client to access user's Microsoft 365 data.
  *
@@ -65,7 +62,7 @@ export default async function run(
     };
   }
 
-  // Construct credential.
+  // Construct teamsfx.
   let teamsfx: TeamsFx;
   try {
     teamsfx = new TeamsFx().setSsoToken(accessToken);
@@ -75,7 +72,7 @@ export default async function run(
       status: 500,
       body: {
         error:
-          "Failed to obtain on-behalf-of credential using your accessToken. " +
+          "Failed to construct TeamsFx using your accessToken. " +
           "Ensure your function app is configured with the right Azure AD App registration.",
       },
     };
