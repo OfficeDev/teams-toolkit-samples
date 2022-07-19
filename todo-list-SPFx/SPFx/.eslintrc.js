66fe977ccc10f86f2eb7123031ce23fc20c29d03
Huihui Wu <huihuiwu@microsoft.com>
2022-07-19 12:49:39 +0800

    chore: disable eslint rules temporarily

--- todo-list-SPFx/SPFx/.eslintrc.js
+++ todo-list-SPFx/SPFx/.eslintrc.js
@@ -1,5 +1,20 @@
 require('@rushstack/eslint-config/patch/modern-module-resolution');
   module.exports = {
     extends: ['@microsoft/eslint-config-spfx/lib/profiles/react'],
-    parserOptions: { tsconfigRootDir: __dirname }
+    parserOptions: { tsconfigRootDir: __dirname },
+    rules: {
+      "@typescript-eslint/naming-convention": "off",
+      "@typescript-eslint/typedef": "off",
+      "@typescript-eslint/explicit-member-accessibility": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@microsoft/spfx/no-async-await": "off",
+      "@typescript-eslint/no-floating-promises": "off",
+      "@typescript-eslint/no-for-in-array": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-member-accessibility": "off",
+      "guard-for-in": "off",
+      "no-var": "off",
+      "prefer-const": "off",
+      "react/jsx-no-bind": "off"
+    }
   };
