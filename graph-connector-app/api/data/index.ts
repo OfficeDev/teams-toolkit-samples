103ebf9d2c23d6206de6d914fcd365aea5f8065c
Jun Han <junhan@microsoft.com>
2022-08-03 10:02:08 +0800

    fix(graph-connector): rollback the workaround fix (#447)

--- graph-connector-app/api/data/index.ts
+++ graph-connector-app/api/data/index.ts
@@ -72,7 +72,7 @@ export default async function run(
             "partNumber": Number(item.PartNumber),
             "name": item.Name,
             "description": item.Description,
-            "price": item.Price,
+            "price": Number(item.Price),
             "inventory": Number(item.Inventory),
             "appliances": item.Appliances.split(";"),
             "appliances@odata.type": "Collection(String)"
