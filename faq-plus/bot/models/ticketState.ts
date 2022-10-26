929c3f6353085b157042f77704faa144eac0f84b
Chaoyi Yuan <blackchoey@gmail.com>
2022-10-26 16:17:29 +0800

    chore: remove FAQ Plus sample (#520)

--- faq-plus/bot/models/ticketState.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export enum TicketState {
-  Open = 0,
-  Closed = 1,
-  MaxValue = Closed,
-}
--- faq-plus/configapp/Microsoft.Teams.Apps.FAQPlusPlus.Configuration/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Microsoft.Teams.Apps.FAQPlusPlus.Configuration.MvcApplication" Language="C#" %>
