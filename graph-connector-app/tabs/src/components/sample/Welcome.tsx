ffaa0de10fa01d95c3130e1ead3344186f93aba2
Yiqing Zhao <cissyzhao99@gmail.com>
2022-11-07 10:27:54 +0800

    feat: update samples to use TeamsFx and TeamsFx-React V2 (#524)
    * feat: update package.json
    
    * feat: update auth-end.html and App
    
    * feat: update connector app
    
    * feat: update graph connector app
    
    * feat: update package.json
    
    * feat: update sample name
    
    * Update OneProductivityHubAspNetCore.csproj
    
    * Update TeamsJsBlazorInterop.js
    
    * Update TeamsJsBlazorInterop.js
    
    * feat: revert auth-end.html to align with templates
    
    * chore: update graph connector app

--- graph-connector-app/tabs/src/components/sample/Welcome.tsx
+++ graph-connector-app/tabs/src/components/sample/Welcome.tsx
@@ -1,40 +1,23 @@
+import { useContext } from "react";
 import { Button, Image, Loader } from "@fluentui/react-northstar";
 import "./Welcome.css";
 import { Introduce } from "./Introduce";
 import { Ingest } from "./Ingest";
-import { useTeamsFx } from "./lib/useTeamsFx";
-import { TeamsFx } from "@microsoft/teamsfx";
-import { useData } from "./lib/useData";
+import { useGraph } from "@microsoft/teamsfx-react";
 import { Query } from "./Query";
-import { useLogin } from "./lib/useLogin";
 import { Scopes } from "./lib/constants";
+import { TeamsFxContext } from "../Context";
 
 export function Welcome() {
-  let { needLogin, loading, reload } = useLogin(Scopes);
-  const { isInTeams } = useTeamsFx();
-  const userProfile = useData(async () => {
-    const teamsfx = new TeamsFx();
-    return isInTeams ? await teamsfx.getUserInfo() : undefined;
-  })?.data;
-  const userName = userProfile ? userProfile.displayName : "";
-
-  async function login() {
-    try {
-      const teamsfx = new TeamsFx();
-      await teamsfx.login(Scopes);
-      reload();
-    } catch (err: any) {
-      if (err.message?.includes("CancelledByUser")) {
-        const helpLink = "https://aka.ms/teamsfx-auth-code-flow";
-        err.message +=
-          "\nIf you see \"AADSTS50011: The reply URL specified in the request does not match the reply URLs configured for the application\" " +
-          "in the popup window, you may be using unmatched version for TeamsFx SDK (version >= 0.5.0) and Teams Toolkit (version < 3.3.0) or " +
-          `cli (version < 0.11.0). Please refer to the help link for how to fix the issue: ${helpLink}`;
-      }
-
-      alert("Login failed: " + err);
-    }
-  }
+  const { teamsfx } = useContext(TeamsFxContext);
+  const { loading, error, data, reload } = useGraph(
+    async (graph, teamsfx, scope) => {
+      // Call graph api directly to get user profile information
+      const profile = await graph.api("/me").get();
+      return { profile };
+    },
+    { scope: Scopes, teamsfx: teamsfx }
+  );
 
   return (
     <div>
@@ -43,12 +26,14 @@ export function Welcome() {
           <Loader style={{ margin: 100 }} />
         </div>
       )}
-      {!loading && needLogin === false && (
+      {!loading && data && (
         <div className="welcome page">
           <div className="narrow page-padding">
             <Image src="hello.png" />
-            <h1 className="center">Hello{userName ? ", " + userName : ""}!</h1>
-            <p className="center">Let's build your first custom Microsoft Graph connector.</p>
+            <h1 className="center">Hello, {data.profile.displayName}!</h1>
+            <p className="center">
+              Let's build your first custom Microsoft Graph connector.
+            </p>
             <div className="sections">
               <Introduce />
               <Ingest />
@@ -57,10 +42,18 @@ export function Welcome() {
           </div>
         </div>
       )}
-      {!loading && needLogin && <div className="auth">
-        <h2>Welcome to Graph Connector App!</h2>
-        <Button primary onClick={() => login()}>Start</Button>
-      </div>}
+      {!loading && !data && (
+        <div className="auth">
+          <h2>Welcome to Graph Connector App!</h2>
+          <Button primary content="Start" disabled={loading} onClick={reload} />
+        </div>
+      )}
+      {!loading && error && (
+        <div className="error">
+          Failed to get your profile. Please try again later. <br /> Details:{" "}
+          {error.toString()}
+        </div>
+      )}
     </div>
   );
 }
