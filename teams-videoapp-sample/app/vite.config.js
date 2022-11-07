fa1cc3ce643f68d62cbd5fdaf30e4de7848a3bab
Qinen Zhu <qinzhu@microsoft.com>
2022-11-07 16:05:44 +0800

    chore: update vite config and package info

--- teams-videoapp-sample/app/vite.config.js
+++ teams-videoapp-sample/app/vite.config.js
@@ -1,13 +1,26 @@
-import mkcert from 'vite-plugin-mkcert'
+import dns from "dns";
+
+// use localhost by default, see https://vitejs.dev/config/server-options.html#server-host
+dns.setDefaultResultOrder('verbatim');
+
+const SSL_KEY_FILE = process.env["SSL_KEY_FILE"];
+const SSL_CRT_FILE = process.env["SSL_CRT_FILE"];
+
+let options = undefined;
+if (SSL_KEY_FILE && SSL_CRT_FILE) {
+    options = {
+        key: SSL_KEY_FILE,
+        cert: SSL_CRT_FILE
+    }
+}
+
 export default {
     base: '/teams-videoapp-sample/app/',
     build: {
         outDir: './dist/app'
     },
-    plugins: [
-        mkcert()
-    ],
     server: {
-        https: true
+        https: options || false,
+        port: process.env["PORT"] || "53000"
     }
 }
