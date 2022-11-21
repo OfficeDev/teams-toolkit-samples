095f08e541e8a6656c461d14fdee5d9372ddd543
Jun Han <junhan@microsoft.com>
2022-11-21 14:00:12 +0800

    chore(app-setting): command-bot-with-sso

--- command-bot-with-sso/bot/src/internal/initialize.ts
+++ command-bot-with-sso/bot/src/internal/initialize.ts
@@ -3,6 +3,7 @@ import {ConversationBot} from "@microsoft/teamsfx";
 import { HelloWorldCommandHandler } from "../helloworldCommandHandler";
 import { ProfileSsoCommandHandler } from "../profileSsoCommandHandler";
 import { PhotoSsoCommandHandler } from "../photoSsoCommandHandler";
+import config from "./config";
 
 // Create the command bot and register the command handlers for your app.
 // You can also use the commandBot.command.registerCommands to register other commands
@@ -12,8 +13,8 @@ export const commandBot = new ConversationBot({
   // The bot id and password to create BotFrameworkAdapter.
   // See https://aka.ms/about-bot-adapter to learn more about adapters.
   adapterConfig: {
-    appId: process.env.BOT_ID,
-    appPassword: process.env.BOT_PASSWORD,
+    appId: config.botId,
+    appPassword: config.botPassword,
   },
 
   // See https://docs.microsoft.com/microsoftteams/platform/toolkit/teamsfx-sdk to learn more about ssoConfig
