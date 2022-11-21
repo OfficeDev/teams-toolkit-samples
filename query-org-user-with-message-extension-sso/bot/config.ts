6c61827825d4faea8e06cff7acd5a4d20141e4cf
Jun Han <junhan@microsoft.com>
2022-11-21 14:28:14 +0800

    chore(app-setting): query-org-user-with-message-extension-sso

---
+++ query-org-user-with-message-extension-sso/bot/config.ts
@@ -0,0 +1,10 @@
+const config = {
+  botId: process.env.BOT_ID,
+  botPassword: process.env.BOT_PASSWORD,
+  authorityHost: process.env.M365_AUTHORITY_HOST,
+  clientId: process.env.M365_CLIENT_ID,
+  tenantId: process.env.M365_TENANT_ID,
+  clientSecret: process.env.M365_CLIENT_SECRET,
+};
+
+export default config;
