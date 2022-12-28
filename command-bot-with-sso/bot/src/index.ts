0dee2639193abb08aa34b9b038ac117548095fe5
Qianhao Dong <qidon@microsoft.com>
2022-12-28 14:27:22 +0800

    fix: fix restify bot (v3) (#589)

--- command-bot-with-sso/bot/src/index.ts
+++ command-bot-with-sso/bot/src/index.ts
@@ -27,7 +27,7 @@ server.post("/api/messages", async (req, res) => {
 });
 
 server.get(
-  "/auth-*.html",
+  "/auth-:name(start|end).html",
   restify.plugins.serveStatic({
     directory: path.join(__dirname, "public"),
   })
