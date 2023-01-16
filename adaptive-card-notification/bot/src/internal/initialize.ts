5052a396a2244ca0a3fac7143ee2da69202cda0a
Yiqing Zhao <cissyzhao99@gmail.com>
2023-01-16 13:25:01 +0800

    chore: update adaptive card notification

--- adaptive-card-notification/bot/src/internal/initialize.ts
+++ adaptive-card-notification/bot/src/internal/initialize.ts
@@ -1,13 +1,15 @@
-import { ConversationBot } from "@microsoft/teamsfx";
+import { BotBuilderCloudAdapter } from "@microsoft/teamsfx";
+import ConversationBot = BotBuilderCloudAdapter.ConversationBot;
 import { BlobsStorage } from "../storage/blobsStorage";
 
 // Create bot.
 export const bot = new ConversationBot({
-  // The bot id and password to create BotFrameworkAdapter.
+  // The bot id and password to create CloudAdapter.
   // See https://aka.ms/about-bot-adapter to learn more about adapters.
   adapterConfig: {
-    appId: process.env.BOT_ID,
-    appPassword: process.env.BOT_PASSWORD,
+    MicrosoftAppId: process.env.BOT_ID,
+    MicrosoftAppPassword: process.env.BOT_PASSWORD,
+    MicrosoftAppType: "MultiTenant",
   },
   // Enable notification
   notification: {
