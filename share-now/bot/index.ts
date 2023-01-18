ab6e794b7924bc355d1bd91f6daa58fce812ee3b
Yiqing Zhao <cissyzhao99@gmail.com>
2023-01-18 10:24:40 +0800

    chore: update share-now

--- share-now/bot/index.ts
+++ share-now/bot/index.ts
@@ -4,43 +4,63 @@ import * as restify from 'restify';
 
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
-import { BotFrameworkAdapter } from 'botbuilder';
+import {
+    CloudAdapter,
+    ConfigurationServiceClientCredentialFactory,
+    ConfigurationBotFrameworkAuthentication,
+    TurnContext,
+  } from "botbuilder";
 
 // This bot's main dialog.
 import { MessageExtensionBot } from './messageExtensionBot';
 
 // Create HTTP server.
 const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
 server.listen(process.env.port || process.env.PORT || 3978, () => {
     console.log(`\n${server.name} listening to ${server.url}`);
 });
 
 // Create adapter.
 // See https://aka.ms/about-bot-adapter to learn more about adapters.
-const adapter = new BotFrameworkAdapter({
-    appId: process.env.BOT_ID,
-    appPassword: process.env.BOT_PASSWORD
-});
+const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
+    MicrosoftAppId: process.env.BOT_ID,
+    MicrosoftAppPassword: process.env.BOT_PASSWORD,
+    MicrosoftAppType: "MultiTenant",
+  });
+  
+  const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(
+    {},
+    credentialsFactory
+  );
+  
+  const adapter = new CloudAdapter(botFrameworkAuthentication);
 
 // Catch-all for errors.
-const onTurnErrorHandler = async (context, error) => {
+// Catch-all for errors.
+const onTurnErrorHandler = async (context: TurnContext, error: Error) => {
     // This check writes out errors to console log .vs. app insights.
     // NOTE: In production environment, you should consider logging this to Azure
     //       application insights.
     console.error(`\n [onTurnError] unhandled error: ${error}`);
-
+  
     // Send a trace activity, which will be displayed in Bot Framework Emulator
     await context.sendTraceActivity(
-        'OnTurnError Trace',
-        `${error}`,
-        'https://www.botframework.com/schemas/error',
-        'TurnError'
+      "OnTurnError Trace",
+      `${error}`,
+      "https://www.botframework.com/schemas/error",
+      "TurnError"
     );
-
+  
     // Send a message to the user
-    await context.sendActivity('The bot encountered an error or bug.');
-    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
-};
+    await context.sendActivity(
+      `The bot encountered unhandled error:\n ${error.message}`
+    );
+    await context.sendActivity(
+      "To continue to run this bot, please fix the bot source code."
+    );
+  };
+  
 
 // Set the onTurnError for the singleton BotFrameworkAdapter.
 adapter.onTurnError = onTurnErrorHandler;
@@ -49,8 +69,8 @@ adapter.onTurnError = onTurnErrorHandler;
 const messageExtensionBot = new MessageExtensionBot();
 
 // Listen for incoming requests.
-server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
+server.post('/api/messages', (req, res, next) => {
+    adapter.process(req, res, async (context) => {
         // Route to main dialog.
         await messageExtensionBot.run(context);
     });
