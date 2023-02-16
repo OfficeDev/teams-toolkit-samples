87af076bb24aeb54648832c3d26596d260544683
huimiao <huimiao@microsoft.com>
2023-02-16 17:47:19 +0800

    chore: change style management

--- team-central-dashboard/src/views/lib/Dashboard.styles.ts
+++ team-central-dashboard/src/views/lib/Dashboard.styles.ts
@@ -1,4 +1,9 @@
-import { CSSProperties } from "react";
+import { mergeStyles, mergeStyleSets } from "@fluentui/react";
+
+export interface IDashboardClassNames {
+  root: string;
+  mobile: string;
+}
 
 /**
  * Defines the styles for the Dashboard component.
@@ -8,18 +13,28 @@ import { CSSProperties } from "react";
  * @param columnWidths The value is a string that can be split by a space character to define the width of each column. For example, "1fr 2fr 2fr" defines three columns with the first column occupying 20% of the available space, the second and third columns each occupying 40% of the available space.
  * @returns The styles for the Dashboard component.
  */
-export const dashboardStyles = (
-  isMobile?: boolean,
+export const getClassName = (
   rowHeighs?: string,
   columnWidths?: string
-): CSSProperties => {
-  return {
-    display: "grid",
-    gap: "20px",
-    padding: "20px",
-    gridTemplateColumns: isMobile ? "1fr" : columnWidths ?? "4fr 6fr",
-    gridTemplateRows: rowHeighs ?? "1fr",
-  };
+): IDashboardClassNames => {
+  return mergeStyleSets({
+    root: {
+      display: "grid",
+      gap: "20px",
+      padding: "20px",
+      ...(rowHeighs !== undefined ? { gridTemplateRows: rowHeighs } : { gridTemplateRows: "1fr" }),
+      ...(columnWidths !== undefined
+        ? { gridTemplateColumns: columnWidths }
+        : { gridTemplateColumns: "4fr 6fr" }),
+    },
+    mobile: {
+      display: "grid",
+      gap: "20px",
+      padding: "20px",
+      gridTemplateColumns: "1fr",
+      gridTemplateRows: "1fr",
+    },
+  });
 };
 
 /**
@@ -29,13 +44,11 @@ export const dashboardStyles = (
  * @param width The width of the column.
  * @returns The styles for the column.
  */
-export const oneColumn = (heights?: string, width?: string): CSSProperties => {
-  const templateRows: CSSProperties = heights ? { gridTemplateRows: heights ?? "1fr" } : {};
-  return {
+export const oneColumn = (heights?: string, width?: string): string => {
+  return mergeStyles({
     display: "grid",
     gap: "20px",
-    gridTemplateRows: heights ?? "1fr",
-    gridAutoColumns: width ?? "1fr",
-    ...templateRows,
-  };
+    ...(heights !== undefined ? { gridTemplateRows: heights } : { gridTemplateRows: "1fr" }),
+    ...(width !== undefined ? { gridTemplateColumns: width } : { gridTemplateColumns: "1fr" }),
+  });
 };
