89f528eb7be718b1534613612da86f99ec186a6b
Ayca Bas <aycabas@microsoft.com>
2023-03-07 16:07:42 +0400

    dev-assist-dashboard

---
+++ developer-assist-dashboard/src/configs.ts
@@ -0,0 +1,11 @@
+// devOps configurations
+export const ORGANIZATION_NAME =;
+export const PROJECT_NAME =;
+export const DEVOPS_PERSONAL_ACCESS_TOKEN =;
+// github configurations
+export const REPOSITORY_OWNER_NAME =;
+export const REPOSITORY_NAME =;
+export const GITHUB_PERSONAL_ACCESS_TOKEN =;
+// Plan configurations
+export const PLAN_ID =;
+export const BUCKET_ID =;
