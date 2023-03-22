9c6dbed156f03ec6e41e1e53833e8e8f104a1236
Yiqing Zhao <cissyzhao99@gmail.com>
2023-03-22 15:50:29 +0800

    feat: update hello-world-meeting to use fluentui v9 and react 18

--- hello-world-in-meeting/src/components/Tab.jsx
+++ hello-world-in-meeting/src/components/Tab.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { app, teamsCore } from "@microsoft/teams-js";
-import MediaQuery from 'react-responsive';
-import './App.css';
+import MediaQuery from "react-responsive";
+import "./App.css";
 
 class Tab extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      context: {}
-    }
+      context: {},
+    };
   }
 
   //React lifecycle method that gets called once a component has finished mounting
   //Learn more: https://reactjs.org/docs/react-component.html#componentdidmount
-  componentDidMount(){
+  componentDidMount() {
     app.initialize().then(() => {
       // Notifies that the app initialization is successfully and is ready for user interaction.
       app.notifySuccess();
@@ -22,11 +22,11 @@ class Tab extends React.Component {
       app.getContext().then(async (context) => {
         this.setState({
           meetingId: context.meeting.id,
-          userName: context.user.userPrincipalName
+          userName: context.user.userPrincipalName,
         });
 
         // Enable app caching.
-        // App Caching was configured in this sample to reduce the reload time of your app in a meeting. 
+        // App Caching was configured in this sample to reduce the reload time of your app in a meeting.
         // To learn about limitations and available scopes, please check https://learn.microsoft.com/en-us/microsoftteams/platform/apps-in-teams-meetings/app-caching-for-your-tab-app.
         if (context.page.frameContext === "sidePanel") {
           teamsCore.registerOnLoadHandler((context) => {
@@ -34,8 +34,8 @@ class Tab extends React.Component {
             app.notifySuccess();
           });
 
-          teamsCore.registerBeforeUnloadHandler((readyToUnload: any) => {
-            // Dispose resources here if necessary. 
+          teamsCore.registerBeforeUnloadHandler((readyToUnload) => {
+            // Dispose resources here if necessary.
             // Notify readiness by invoking readyToUnload.
             readyToUnload();
             return true;
@@ -51,17 +51,19 @@ class Tab extends React.Component {
     let userPrincipleName = this.state.userName ?? "";
 
     return (
-    <div>
-      <h1>In-meeting app sample</h1>
-      <h3>Principle Name:</h3>
-      <p>{userPrincipleName}</p>
-      <h3>Meeting ID:</h3>
-      <p>{meetingId}</p>
-      <MediaQuery maxWidth={280}>
-        <h3>This is the side panel</h3>
-        <a href="https://docs.microsoft.com/en-us/microsoftteams/platform/apps-in-teams-meetings/teams-apps-in-meetings">Need more info, open this document in new tab or window.</a>
-      </MediaQuery>
-    </div>
+      <div>
+        <h1>In-meeting app sample</h1>
+        <h3>Principle Name:</h3>
+        <p>{userPrincipleName}</p>
+        <h3>Meeting ID:</h3>
+        <p>{meetingId}</p>
+        <MediaQuery maxWidth={280}>
+          <h3>This is the side panel</h3>
+          <a href="https://docs.microsoft.com/en-us/microsoftteams/platform/apps-in-teams-meetings/teams-apps-in-meetings">
+            Need more info, open this document in new tab or window.
+          </a>
+        </MediaQuery>
+      </div>
     );
   }
 }
