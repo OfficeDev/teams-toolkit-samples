a7bdae60e19d9e35958f2f745a93db65abb67fae
Yiqing Zhao <cissyzhao99@gmail.com>
2023-04-03 15:40:34 +0800

    feat: migrate todo list samples to use Fluent UI and React v9 (#804)
    * feat: update hello-world-tab-with-backend to use fluentui v9 and react 18
    
    * feat: update bot with tab to use fluentui v9 and react 18
    
    * feat: update tab office-addin to use fluentui v9 and react 18
    
    * feat: update hello-world-meeting to use fluentui v9 and react 18
    
    * feat: update todo list m365
    
    * feat: update todo list with azure backend
    
    * feat: remove package-lock.json

--- todo-list-with-Azure-backend/tabs/src/components/Tab.js
+++ todo-list-with-Azure-backend/tabs/src/components/Tab.js
@@ -1,19 +1,33 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import React from 'react';
-import './App.css';
-import './Tab.css'
-import { BearerTokenAuthProvider, createApiClient, TeamsUserCredential } from "@microsoft/teamsfx";
+import React from "react";
+import "./App.css";
+import "./Tab.css";
+import {
+  BearerTokenAuthProvider,
+  createApiClient,
+  TeamsUserCredential,
+} from "@microsoft/teamsfx";
 import Profile from "./Profile";
 import Creator from "./Creator";
-import { Checkbox, Button, Input, MenuButton } from "@fluentui/react-northstar"
-import noItemimage from '../images/no-item.png'
-import { app } from '@microsoft/teams-js';
-import config from './lib/config';
+import {
+  Checkbox,
+  Button,
+  Input,
+  Menu,
+  MenuTrigger,
+  MenuButton,
+  MenuPopover,
+  MenuList,
+  MenuItem,
+} from "@fluentui/react-components";
+import { Notepad20Regular, ContactCard20Regular } from "@fluentui/react-icons";
+import noItemimage from "../images/no-item.png";
+import { app } from "@microsoft/teams-js";
+import config from "./lib/config";
 
 class Tab extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -22,8 +36,8 @@ class Tab extends React.Component {
       newItemDescription: "",
       showLoginPage: undefined,
       isAddingItem: false,
-      initialized: false
-    }
+      initialized: false,
+    };
   }
 
   async componentDidMount() {
@@ -36,12 +50,12 @@ class Tab extends React.Component {
       clientId: config.clientId,
       initiateLoginEndpoint: config.initiateLoginEndpoint,
     };
-    
+
     const credential = new TeamsUserCredential(authConfig);
     const userInfo = await credential.getUserInfo();
 
     this.setState({
-      userInfo: userInfo
+      userInfo: userInfo,
     });
 
     this.scope = ["User.Read", "User.ReadBasic.All"];
@@ -52,12 +66,15 @@ class Tab extends React.Component {
     // createApiClient(...) creates an Axios instance which uses BearerTokenAuthProvider to inject token to request header
     const apiClient = createApiClient(
       apiBaseUrl,
-      new BearerTokenAuthProvider(async () => (await credential.getToken("")).token));
+      new BearerTokenAuthProvider(
+        async () => (await credential.getToken("")).token
+      )
+    );
     this.apiClient = apiClient;
   }
 
   async initData() {
-    if (!await this.checkIsConsentNeeded()) {
+    if (!(await this.checkIsConsentNeeded())) {
       await this.getItems();
     }
   }
@@ -70,9 +87,9 @@ class Tab extends React.Component {
       if (err instanceof Error && err.message?.includes("CancelledByUser")) {
         const helpLink = "https://aka.ms/teamsfx-auth-code-flow";
         err.message +=
-          "\nIf you see \"AADSTS50011: The reply URL specified in the request does not match the reply URLs configured for the application\" " +
+          '\nIf you see "AADSTS50011: The reply URL specified in the request does not match the reply URLs configured for the application" ' +
           "in the popup window, you may be using unmatched version for TeamsFx SDK (version >= 0.5.0) and Teams Toolkit (version < 3.3.0) or " +
-          `cli (version < 0.11.0). Please refer to the help link for how to fix the issue: ${helpLink}` ;
+          `cli (version < 0.11.0). Please refer to the help link for how to fix the issue: ${helpLink}`;
       }
 
       alert("Login failed: " + err);
@@ -86,12 +103,12 @@ class Tab extends React.Component {
       await this.credential.getToken(this.scope);
     } catch (error) {
       this.setState({
-        showLoginPage: true
+        showLoginPage: true,
       });
       return true;
     }
     this.setState({
-      showLoginPage: false
+      showLoginPage: false,
     });
     return false;
   }
@@ -122,7 +139,7 @@ class Tab extends React.Component {
         method: method,
         url: command,
         data: options,
-        params
+        params,
       });
       message = response.data;
     } catch (err) {
@@ -152,9 +169,16 @@ class Tab extends React.Component {
 
   async getItems() {
     // Use client TeamsFx SDK to call "todo" Azure Function in "get" method to get all todo list which belong to user oid
-    let result = await this.callFunctionWithErrorHandling("todo", "get", undefined, { channelOrChatId: this.channelOrChatId });
+    let result = await this.callFunctionWithErrorHandling(
+      "todo",
+      "get",
+      undefined,
+      { channelOrChatId: this.channelOrChatId }
+    );
     if ("Error" === result) {
-      throw new Error("todo Function failed, please check Azure Functions log for details!");
+      throw new Error(
+        "todo Function failed, please check Azure Functions log for details!"
+      );
     } else {
       this.setState({
         items: result,
@@ -167,30 +191,35 @@ class Tab extends React.Component {
     const newItems = JSON.parse(JSON.stringify(this.state.items));
     newItems.push({
       description: this.state.newItemDescription,
-      objectId: this.state.userInfo.objectId
-    })
+      objectId: this.state.userInfo.objectId,
+    });
     this.setState({
       newItemDescription: "",
-      items: newItems
+      items: newItems,
     });
 
     // Use client TeamsFx SDK to call "todo" Azure Function in "post" method to insert a new todo item under user oid
     await this.callFunctionWithErrorHandling("todo", "post", {
-      description: this.state.newItemDescription, isCompleted: false, channelOrChatId: this.channelOrChatId
+      description: this.state.newItemDescription,
+      isCompleted: false,
+      channelOrChatId: this.channelOrChatId,
     });
     this.refresh();
   }
 
   async onUpdateItem(id, description) {
     // Use client TeamsFx SDK to call "todo" Azure Function in "put" method to update a todo item
-    await this.callFunctionWithErrorHandling("todo", "put", { id, description });
+    await this.callFunctionWithErrorHandling("todo", "put", {
+      id,
+      description,
+    });
   }
 
   async onDeleteItem(id) {
-    const newItems = this.state.items.filter(item => item.id !== id);
+    const newItems = this.state.items.filter((item) => item.id !== id);
     this.setState({
-      items: newItems
-    })
+      items: newItems,
+    });
     // Use client TeamsFx SDK to call "todo" Azure Function in "delete" method to delete a todo item
     await this.callFunctionWithErrorHandling("todo", "delete", { id });
   }
@@ -198,15 +227,18 @@ class Tab extends React.Component {
   async onCompletionStatusChange(id, index, isCompleted) {
     this.handleInputChange(index, "isCompleted", isCompleted);
     // Use client TeamsFx SDK to call "todo" Azure Function in "put" method to update a todo item to completed
-    await this.callFunctionWithErrorHandling("todo", "put", { id, isCompleted });
+    await this.callFunctionWithErrorHandling("todo", "put", {
+      id,
+      isCompleted,
+    });
   }
 
   handleInputChange(index, property, value) {
-    const newItems = JSON.parse(JSON.stringify(this.state.items))
+    const newItems = JSON.parse(JSON.stringify(this.state.items));
     newItems[index][property] = value;
     this.setState({
-      items: newItems
-    })
+      items: newItems,
+    });
   }
 
   async refresh() {
@@ -214,132 +246,161 @@ class Tab extends React.Component {
   }
 
   render() {
-    const items = this.state.items?.map((item, index) =>
+    const items = this.state.items?.map((item, index) => (
       <div key={index} className="item">
         <div className="complete">
           <Checkbox
             checked={this.state.items[index].isCompleted}
-            onChange={(e, { checked }) => this.onCompletionStatusChange(item.id, index, checked)}
+            onChange={(e, { checked }) =>
+              this.onCompletionStatusChange(item.id, index, checked)
+            }
             className="is-completed-input"
           />
         </div>
         <div className="description">
           <Input
             value={this.state.items[index].description}
-            onChange={(e) => this.handleInputChange(index, "description", e.target.value)}
+            onChange={(e) =>
+              this.handleInputChange(index, "description", e.target.value)
+            }
             onKeyDown={(e) => {
-              if (e.key === 'Enter') {
+              if (e.key === "Enter") {
                 this.onUpdateItem(item.id, this.state.items[index].description);
                 e.target.blur();
               }
             }}
-            onBlur={() => this.onUpdateItem(item.id, this.state.items[index].description)}
-            className={"text" + (this.state.items[index].isCompleted ? " is-completed" : "")}
+            onBlur={() =>
+              this.onUpdateItem(item.id, this.state.items[index].description)
+            }
+            className={
+              "text" +
+              (this.state.items[index].isCompleted ? " is-completed" : "")
+            }
           />
         </div>
-        <Creator objectId={item.objectId} teamsUserCredential={this.credential} scope={this.scope} />
+        <Creator
+          objectId={item.objectId}
+          teamsUserCredential={this.credential}
+          scope={this.scope}
+        />
         <div className="action">
-          <MenuButton
-            trigger={<Button content="..." />}
-            menu={[
-              {
-                content: 'Delete',
-                onClick: () => this.onDeleteItem(item.id)
-              }
-            ]}
-            on="click"
-            unstable_pinned={true}
-          />
+          <Menu>
+            <MenuTrigger disableButtonEnhancement>
+              <MenuButton appearance="subtle" menuIcon={null}>
+                ...
+              </MenuButton>
+            </MenuTrigger>
+            <MenuPopover>
+              <MenuList>
+                <MenuItem onClick={() => this.onDeleteItem(item.id)}>
+                  Delete
+                </MenuItem>
+              </MenuList>
+            </MenuPopover>
+          </Menu>
         </div>
       </div>
-    );
+    ));
 
     return (
       <div>
-        {this.state.showLoginPage === false && <div className="flex-container">
-          <div className="todo-col">
-            <div className="todo">
-              <div className="header">
-                <div className="title">
-                  <h2>To Do List</h2>
-                  <span>{this.state.items.length} item{this.state.items.length === 1 ? "" : "s"}</span>
+        {this.state.showLoginPage === false && (
+          <div className="flex-container">
+            <div className="todo-col">
+              <div className="todo">
+                <div className="header">
+                  <div className="title">
+                    <h2>To Do List</h2>
+                    <span>
+                      {this.state.items.length} item
+                      {this.state.items.length === 1 ? "" : "s"}
+                    </span>
+                  </div>
+
+                  <div className="add-button">
+                    <Button
+                      appearance="primary"
+                      onClick={() => this.setState({ isAddingItem: true })}
+                    >
+                      + Add task
+                    </Button>
+                  </div>
                 </div>
 
-                <div className="add-button">
-                  <Button primary onClick={() => this.setState({ isAddingItem: true })}>+ Add task</Button>
-                </div>
+                {this.state.items.length > 0 && (
+                  <div className="header-container">
+                    <div className="note">
+                      <Notepad20Regular />
+                      <span>Note</span>
+                    </div>
+                    <div className="created-by">
+                      <ContactCard20Regular />
+                      <span>Created By</span>
+                    </div>
+                  </div>
+                )}
+
+                {items}
+
+                {this.state.isAddingItem && (
+                  <div className="item add">
+                    <div className="complete">
+                      <Checkbox disabled className="is-completed-input" />
+                    </div>
+                    <div className="description">
+                      <Input
+                        autoFocus
+                        type="text"
+                        value={this.state.newItemDescription}
+                        onChange={(e) =>
+                          this.setState({ newItemDescription: e.target.value })
+                        }
+                        onKeyDown={(e) => {
+                          if (e.key === "Enter") {
+                            this.onAddItem();
+                          }
+                        }}
+                        onBlur={() => {
+                          if (this.state.newItemDescription) {
+                            this.onAddItem();
+                          }
+                          this.setState({
+                            isAddingItem: false,
+                          });
+                        }}
+                        className="text"
+                      />
+                    </div>
+                  </div>
+                )}
+
+                {this.state.initialized &&
+                  !this.state.items.length &&
+                  !this.state.isAddingItem && (
+                    <div className="no-item">
+                      <div>
+                        <img src={noItemimage} alt="no item" />
+                      </div>
+                      <div>
+                        <h2>No tasks</h2>
+                        <p>Add more tasks to make you day productive.</p>
+                      </div>
+                    </div>
+                  )}
               </div>
-
-              {this.state.items.length > 0 && <div className="header-container">
-                <div className="note">
-                  <svg width="12" height="16" viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.0312 1.00781H12V16H0V1.00781H0.984375V0H1.98438V1.00781H4V0H5V1.00781H7.01562V0H8.01562V1.00781H10.0312V0H11.0312V1.00781ZM11 15V2.00781H1V15H11ZM9 4.00781V5.00781H3V4.00781H9ZM3 13.0156V12.0156H9V13.0156H3ZM3 9.00781V8.00781H9V9.00781H3Z" fill="black" />
-                  </svg>
-                  <span>Note</span>
-                </div>
-                <div className="created-by">
-                  <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M6 4.5C6 5.32843 5.32843 6 4.5 6C3.67157 6 3 5.32843 3 4.5C3 3.67157 3.67157 3 4.5 3C5.32843 3 6 3.67157 6 4.5Z" fill="#242424" />
-                    <path d="M2 7.69879C2 7.17479 2.42479 6.75 2.94879 6.75H6.05121C6.57521 6.75 7 7.17479 7 7.69879C7 8.54603 6.42338 9.28454 5.60144 9.49003L5.54243 9.50478C4.85801 9.67589 4.14199 9.67589 3.45757 9.50478L3.39856 9.49003C2.57661 9.28454 2 8.54603 2 7.69879Z" fill="#242424" />
-                    <path d="M9.5 4C9.22386 4 9 4.22386 9 4.5C9 4.77614 9.22386 5 9.5 5H12.5C12.7761 5 13 4.77614 13 4.5C13 4.22386 12.7761 4 12.5 4H9.5Z" fill="#242424" />
-                    <path d="M9.5 7C9.22386 7 9 7.22386 9 7.5C9 7.77614 9.22386 8 9.5 8H12.5C12.7761 8 13 7.77614 13 7.5C13 7.22386 12.7761 7 12.5 7H9.5Z" fill="#242424" />
-                    <path d="M0 1.75C0 0.783502 0.783502 0 1.75 0H14.25C15.2165 0 16 0.783502 16 1.75V10.25C16 11.2165 15.2165 12 14.25 12H1.75C0.783501 12 0 11.2165 0 10.25V1.75ZM1.75 1C1.33579 1 1 1.33579 1 1.75V10.25C1 10.6642 1.33579 11 1.75 11H14.25C14.6642 11 15 10.6642 15 10.25V1.75C15 1.33579 14.6642 1 14.25 1H1.75Z" fill="#242424" />
-                  </svg>
-                  <span>Created By</span>
-                </div>
-              </div>}
-
-              {items}
-
-              {this.state.isAddingItem && <div className="item add">
-                <div className="complete">
-                  <Checkbox
-                    disabled
-                    className="is-completed-input"
-                  />
-                </div>
-                <div className="description">
-                  <Input
-                    autoFocus
-                    type="text"
-                    value={this.state.newItemDescription}
-                    onChange={(e) => this.setState({ newItemDescription: e.target.value })}
-                    onKeyDown={(e) => {
-                      if (e.key === 'Enter') {
-                        this.onAddItem();
-                      }
-                    }}
-                    onBlur={() => {
-                      if (this.state.newItemDescription) {
-                        this.onAddItem();
-                      }
-                      this.setState({
-                        isAddingItem: false,
-                      });
-                    }}
-                    className="text"
-                  />
-                </div>
-              </div>}
-
-              {this.state.initialized && !this.state.items.length && !this.state.isAddingItem && <div className="no-item">
-                <div>
-                  <img src={noItemimage} alt="no item" />
-                </div>
-                <div>
-                  <h2>No tasks</h2>
-                  <p>Add more tasks to make you day productive.</p>
-                </div>
-              </div>}
             </div>
           </div>
-        </div>}
+        )}
 
-        {this.state.showLoginPage === true && <div className="auth">
-          <Profile userInfo={this.state.userInfo} />
-          <h2>Welcome to To Do List App!</h2>
-          <Button primary onClick={() => this.loginBtnClick()}>Start</Button>
-        </div>}
+        {this.state.showLoginPage === true && (
+          <div className="auth">
+            <Profile userInfo={this.state.userInfo} />
+            <h2>Welcome to To Do List App!</h2>
+            <Button appearance="primary" onClick={() => this.loginBtnClick()}>
+              Start
+            </Button>
+          </div>
+        )}
       </div>
     );
   }
