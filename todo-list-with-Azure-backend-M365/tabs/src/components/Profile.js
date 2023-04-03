0f8b718b3af358d7d97f70b06889fb83f1d89f37
Yiqing Zhao <cissyzhao99@gmail.com>
2023-04-03 15:22:20 +0800

    feat: update todo list m365

--- todo-list-with-Azure-backend-M365/tabs/src/components/Profile.js
+++ todo-list-with-Azure-backend-M365/tabs/src/components/Profile.js
@@ -1,16 +1,16 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import React from 'react';
-import './Profile.css';
-import defaultPhoto from '../images/default-photo.png'
+import React from "react";
+import "./Profile.css";
+import defaultPhoto from "../images/default-photo.png";
 
 class Profile extends React.Component {
   render() {
     return (
       <div className="profile">
         <div className="photo">
-          <img src={defaultPhoto} alt="avatar"/>
+          <img src={defaultPhoto} alt="avatar" />
         </div>
         <div className="info">
           <div className="name">{this.props.userInfo.displayName}</div>
