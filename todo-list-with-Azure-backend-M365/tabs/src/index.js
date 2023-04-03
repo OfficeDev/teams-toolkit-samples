a79f9a6eb2b5884e5ff546f92389ec2ee1be1cdf
Yiqing Zhao <cissyzhao99@gmail.com>
2023-04-03 15:28:47 +0800

    feat: update todo list with azure backend

--- todo-list-with-Azure-backend-M365/tabs/src/index.js
+++ todo-list-with-Azure-backend-M365/tabs/src/index.js
@@ -5,7 +5,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import { FluentProvider, teamsLightTheme } from "@fluentui/react-components"; //https://fluentsite.z22.web.core.windows.net/quick-start
+import { FluentProvider, teamsLightTheme } from "@fluentui/react-components";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
