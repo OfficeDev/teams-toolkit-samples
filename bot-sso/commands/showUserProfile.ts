45d3e6ff67db318f4ce4a2066a3063c86ca38e84
Yiqing Zhao <cissyzhao99@gmail.com>
2023-08-04 15:04:56 +0800

    feat: update bot-sso to use graph sdk

--- bot-sso/commands/showUserProfile.ts
+++ bot-sso/commands/showUserProfile.ts
@@ -1,27 +1,38 @@
-import { ResponseType } from "@microsoft/microsoft-graph-client";
+import { ResponseType, Client } from "@microsoft/microsoft-graph-client";
+import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
 import { CardFactory, TurnContext } from "botbuilder";
-import {
-  createMicrosoftGraphClientWithCredential,
-  OnBehalfOfUserCredential,
-} from "@microsoft/teamsfx";
+import { OnBehalfOfUserCredential } from "@microsoft/teamsfx";
 import { SSOCommand } from "./SSOCommand";
 import oboAuthConfig from "../authConfig";
 
 export class ShowUserProfile implements SSOCommand {
-  commandMessage = 'show';
+  commandMessage = "show";
 
   async operationWithSSOToken(context: TurnContext, ssoToken: string) {
-    await context.sendActivity("Retrieving user information from Microsoft Graph ...");
+    await context.sendActivity(
+      "Retrieving user information from Microsoft Graph ..."
+    );
 
     // Call Microsoft Graph half of user
     const oboCredential = new OnBehalfOfUserCredential(ssoToken, oboAuthConfig);
-    const graphClient = createMicrosoftGraphClientWithCredential(oboCredential, [
-      "User.Read",
-    ]);
+
+    // Create an instance of the TokenCredentialAuthenticationProvider by passing the tokenCredential instance and options to the constructor
+    const authProvider = new TokenCredentialAuthenticationProvider(
+      oboCredential,
+      {
+        scopes: ["User.Read"],
+      }
+    );
+
+    // Initialize Graph client instance with authProvider
+    const graphClient = Client.initWithMiddleware({
+      authProvider: authProvider,
+    });
     const me = await graphClient.api("/me").get();
     if (me) {
       await context.sendActivity(
-        `You're logged in as ${me.displayName} (${me.userPrincipalName})${me.jobTitle ? `; your job title is: ${me.jobTitle}` : ""
+        `You're logged in as ${me.displayName} (${me.userPrincipalName})${
+          me.jobTitle ? `; your job title is: ${me.jobTitle}` : ""
         }.`
       );
 
@@ -49,5 +60,4 @@ export class ShowUserProfile implements SSOCommand {
       );
     }
   }
-
 }
