260eb9c2c306322b9a40083a56b411fa818a66f8
Yiqing Zhao <cissyzhao99@gmail.com>
2023-08-08 14:13:45 +0800

    feat: migrate to use TokenCredentialAuthenticationProvider (#979)

--- command-bot-with-sso/src/photoSsoCommandHandler.ts
+++ command-bot-with-sso/src/photoSsoCommandHandler.ts
@@ -4,9 +4,10 @@ import {
   TriggerPatterns,
   TeamsFxBotSsoCommandHandler,
   TeamsBotSsoPromptTokenResponse,
-  createMicrosoftGraphClientWithCredential,
   OnBehalfOfUserCredential,
 } from "@microsoft/teamsfx";
+import { Client } from "@microsoft/microsoft-graph-client";
+import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
 import oboAuthConfig from "./authConfig";
 
 export class PhotoSsoCommandHandler implements TeamsFxBotSsoCommandHandler {
@@ -15,20 +16,34 @@ export class PhotoSsoCommandHandler implements TeamsFxBotSsoCommandHandler {
   async handleCommandReceived(
     context: TurnContext,
     message: CommandMessage,
-    tokenResponse: TeamsBotSsoPromptTokenResponse,
+    tokenResponse: TeamsBotSsoPromptTokenResponse
   ): Promise<string | Partial<Activity> | void> {
-    await context.sendActivity("Retrieving user information from Microsoft Graph ...");
+    await context.sendActivity(
+      "Retrieving user information from Microsoft Graph ..."
+    );
     // Init OnBehalfOfUserCredential instance with SSO token
-    const oboCredential = new OnBehalfOfUserCredential(tokenResponse.ssoToken, oboAuthConfig);
-    // Add scope for your Azure AD app. For example: Mail.Read, etc.
-    const graphClient = createMicrosoftGraphClientWithCredential(oboCredential,  ["User.Read"]);
+    const oboCredential = new OnBehalfOfUserCredential(
+      tokenResponse.ssoToken,
+      oboAuthConfig
+    );
+    // Create an instance of the TokenCredentialAuthenticationProvider by passing the tokenCredential instance and options to the constructor
+    const authProvider = new TokenCredentialAuthenticationProvider(
+      oboCredential,
+      {
+        scopes: ["User.Read"],
+      }
+    );
+    // Initialize Graph client instance with authProvider
+    const graphClient = Client.initWithMiddleware({
+      authProvider: authProvider,
+    });
 
     // You can add following code to get your photo:
     let photoUrl = "";
     try {
       const photo = await graphClient.api("/me/photo/$value").get();
       const arrayBuffer = await photo.arrayBuffer();
-      const buffer = Buffer.from(arrayBuffer, 'binary');
+      const buffer = Buffer.from(arrayBuffer, "binary");
       photoUrl = "data:image/png;base64," + buffer.toString("base64");
     } catch {
       // Could not fetch photo from user's profile, return empty string as placeholder.
@@ -36,14 +51,14 @@ export class PhotoSsoCommandHandler implements TeamsFxBotSsoCommandHandler {
     if (photoUrl) {
       const photoMessage: Partial<Activity> = {
         type: ActivityTypes.Message,
-        text: 'This is your photo:',
+        text: "This is your photo:",
         attachments: [
           {
-            name: 'photo.png',
-            contentType: 'image/png',
-            contentUrl: photoUrl
-          }
-        ]
+            name: "photo.png",
+            contentType: "image/png",
+            contentUrl: photoUrl,
+          },
+        ],
       };
       return photoMessage;
     } else {
