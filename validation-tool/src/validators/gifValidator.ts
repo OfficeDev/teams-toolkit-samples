ee5ed936047970303aeb66e3281449e84af11a0d
Ning Tang <nintan@microsoft.com>
2023-08-25 11:05:43 +0800

    fix: ut and comment

--- validation-tool/src/validators/gifValidator.ts
+++ validation-tool/src/validators/gifValidator.ts
@@ -27,17 +27,10 @@ export default async function validateGif(projectDir: string): Promise<Result> {
 
   const dimensions = sizeOf(defaultGifPath);
   let valid = false;
-  if (dimensions.height === 920 && dimensions.width === 1600) {
-    valid = true;
-  } else if (dimensions.height === 460 && dimensions.width === 800) {
-    valid = true;
-  } else if (dimensions.width && dimensions.height && dimensions.width / dimensions.height === 40/23) {
-    valid = true;
-  }
-  if (!valid) {
-    result.failed.push(`assets/sampleDemo.gif should have 1600*920/800*460 resolution or same ratio.`);
-  } else {
+  if (dimensions.width && dimensions.height && dimensions.width / dimensions.height === 40/23) {
     result.passed.push(`assets/sampleDemo.gif has 1600*920/800*460 resolution or same ratio.`);
+  } else {
+    result.failed.push(`assets/sampleDemo.gif should have 1600*920/800*460 resolution or same ratio.`);
   }
   return result;
 }
