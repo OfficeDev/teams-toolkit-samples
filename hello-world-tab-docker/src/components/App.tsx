29099060655b8cca0cb4466bd1330243522b15b0
Zhijie Huang false <zhihuan@microsoft.com>
2024-02-28 17:17:32 +0800

    refactor: remove unused pages

--- hello-world-tab-docker/src/components/App.tsx
+++ hello-world-tab-docker/src/components/App.tsx
@@ -51,8 +51,6 @@ export default function App() {
         <Router>
           {!loading && (
             <Routes>
-              <Route path="/privacy" element={<Privacy />} />
-              <Route path="/termsofuse" element={<TermsOfUse />} />
               <Route path="/tab" element={<Tab />} />
               <Route path="/config" element={<TabConfig />} />
               <Route path="*" element={<Navigate to={"/tab"} />}></Route>
