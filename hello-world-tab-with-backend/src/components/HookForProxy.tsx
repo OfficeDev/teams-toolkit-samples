914f5b87f1b1558ffc5d44d71a7d6523df5db45e
xzf0587 <zhaofengxu@microsoft.com>
2024-03-12 10:21:58 +0800

    update: add token generator

--- hello-world-tab-with-backend/src/components/HookForProxy.tsx
+++ hello-world-tab-with-backend/src/components/HookForProxy.tsx
@@ -6,6 +6,7 @@
 
 import { TeamsUserCredential } from "@microsoft/teamsfx";
 import { AccessToken } from "@azure/identity";
+import { Base64 } from "js-base64";
 export function shouldHookForProxy(): boolean {
   return process.env.REACT_APP_HOOK_FOR_PROXY === "true";
 }
@@ -14,9 +15,7 @@ if (shouldHookForProxy()) {
   // hook getToken to return a mocked access token. The token can not be used to call real Graph API.
   TeamsUserCredential.prototype.getToken = async (scopes: string | string[], options?: any) => {
     const accessToken: AccessToken = {
-      // Function extension will check the expiration time of the token, so set it to maximum timestamp.
-      // The function extension will check the azp claim of the token, so set it to a valid value which should be contained in the ALLOWED_APP_IDS in teamsapp.local.yml.
-      token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjlHbW55RlBraGMzaE91UjIybXZTdmduTG83WSJ9.eyJhdWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwL3YyLjAiLCJpYXQiOjE2OTg2NDc2MjUsIm5iZiI6MTY5ODY0NzYyNSwiZXhwIjoyMTQ3NDgzNjQ3LCJhaW8iOiJBVlFBcS84VUFBQUE0TklUR29uUzlrbFBmS0tnZDV5U2JpcE9UbWc2Y2tPZE00MExjUlBuSGpyYWtMSkFvUGZmamZZMnBScGVGQTlJZGkya2Nlcy9GOUhGa0FtS041UDZnUGRwTkRFaWhXa25lUTBjeU1XZEppOD0iLCJhenAiOiI1ZTNjZTZjMC0yYjFmLTQyODUtOGQ0Yi03NWVlNzg3ODczNDYiLCJhenBhY3IiOiIwIiwibmFtZSI6Im1vY2tlZCBHaXZlbiBOYW1lIiwib2lkIjoiMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwIiwicHJlZmVycmVkX3VzZXJuYW1lIjoibW9ja2VkX3VzZXJAbW9ja2VkLm9ubWljcm9zb2Z0LmNvbSIsInJoIjoiMC5BWFlBdGZEb1h0NkxHME9jMVEwbkVVdndiUzlwV1ZOSW5mQkl2YjlsX1hPRkFCcDJBTHMuIiwic2NwIjoiYWNjZXNzX2FzX3VzZXIiLCJzdWIiOiJFM0ZydjNaWEhzbG4yRWVGTU81T2FoekJBdnRZZ0xzZXhaZG9lWDFTYlZrIiwidGlkIjoiMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwIiwidXRpIjoiT2NvZEtjOHBkRUtsTFc1MGlmTk5BQSIsInZlciI6IjIuMCJ9.coSn59IDIfWhwznUz-aIdBVEAZUaGCaulU75zrXCOImOOd37Rtql0lCUovJ5z1ZZBOJxiLU1TwiOHPqHVKwZxucBub3C5oQM6QnXyvIIBtJjMNx7Toocifr4P-toT7k2gnkMTEU5Y-tM5DkShFVvvrW1p0_Phwy8qr7HY6kpMHVcIIcj9M5CdoHXONBTunQGdZPyv-kWi88h3abh9ybADxDLQrdPAkcjqXV_lXZ3tcE2PteXAaM_2NuyiWQDgZrIfshFZjiULTxlR9HsXGF8BukSzH7NHXG6zxtF5IeYMBysUB_V-BHAQ_b1cVNmjwIQVYdrdG0X6DZsT0AhczL47w",
+      token: generateMockedAccessToken(),
       expiresOnTimestamp: 2147483647,
     };
     return accessToken;
@@ -26,4 +25,35 @@ if (shouldHookForProxy()) {
   TeamsUserCredential.prototype.login = async (scopes: string | string[], options?: any) => {
     return;
   };
+}
+
+// Function extension will check the expiration time of the token, so set it to maximum timestamp.
+// The function extension will check the azp claim of the token, so set it to a valid value which should be contained in the ALLOWED_APP_IDS in teamsapp.local.yml.
+function generateMockedAccessToken(): string {
+  const header = {
+    typ: "JWT",
+    alg: "RS256",
+    kid: "9GmnyFPkhc3hOuR22mvSvgnLo7Y"
+  };
+  const payload = {
+    aud: "00000000-0000-0000-0000-000000000000",
+    iss: "https://login.microsoftonline.com/00000000-0000-0000-0000-000000000000/v2.0",
+    iat: 1698647625,
+    nbf: 1698647625,
+    exp: 2147483647,
+    aio: "AVQAq/8UAAAA4NITGonS9klPfKKgd5ySbipOTmg6ckOdM40LcRPnHjrakLJAoPffjfY2pRpeFA9Idi2kces/F9HFkAmKN5P6gPdpNDEihWkneQ0cyMWdJi8=",
+    azp: "5e3ce6c0-2b1f-4285-8d4b-75ee78787346",
+    azpacr: "0",
+    name: "mocked Given Name",
+    oid: "00000000-0000-0000-0000-000000000000",
+    preferred_username: "mocked_user@mocked.onmicrosoft.com",
+    rh: "0.AXYAtfDoXt6LG0Oc1Q0nEUvwbS9pWVNInfBIvb9l_XOFABp2ALs.",
+    scp: "access_as_user",
+    sub: "E3Frv3ZXHsln2EeFMO5OahzBAvtYgLsexZdoeX1SbVk",
+    tid: "00000000-0000-0000-0000-000000000000",
+    uti: "OcodKc8pdEKlLW50ifNNAA",
+    ver: "2.0"
+  };
+  const signature = "mocked signature";
+  return `${Base64.encode(JSON.stringify(header))}.${Base64.encode(JSON.stringify(payload))}.${Base64.encode(signature)}`;
 }
