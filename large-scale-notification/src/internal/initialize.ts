1027feddfd4c3a91e6a536ed159e098c6756c126
yukun-dong <dongyukun1995@163.com>
2024-07-31 09:51:16 +0800

    update samples to be msi

--- large-scale-notification/src/internal/initialize.ts
+++ large-scale-notification/src/internal/initialize.ts
@@ -14,11 +14,7 @@ import ConversationBot = BotBuilderCloudAdapter.ConversationBot;
 export const notificationApp = new ConversationBot({
   // The bot id and password to create CloudAdapter.
   // See https://aka.ms/about-bot-adapter to learn more about adapters.
-  adapterConfig: {
-    MicrosoftAppId: config.botId,
-    MicrosoftAppPassword: config.botPassword,
-    MicrosoftAppType: "MultiTenant",
-  },
+  adapterConfig: config,
   // Enable notification
   notification: {
     enabled: true,
