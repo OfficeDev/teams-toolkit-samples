47618fa4118ad90117066e6c60dddc7c28de7382
Huajie Zhang <zhjay23@qq.com>
2024-12-16 15:45:57 +0800

    Revert "Update the Outlook + Teams sample to Office + Teams sample"

--- hello-world-teams-tab-and-outlook-add-in/add-in/src/taskpane/taskpane.ts
+++ hello-world-teams-tab-and-outlook-add-in/add-in/src/taskpane/taskpane.ts
@@ -9,20 +9,15 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = runOutlook;
+    document.getElementById("run").onclick = run;
   }
 });
 
-export async function runOutlook() {
+export async function run() {
   /**
    * Insert your Outlook code here
    */
 
   const item = Office.context.mailbox.item;
-  let insertAt = document.getElementById("item-subject");
-  let label = document.createElement("b").appendChild(document.createTextNode("Subject: "));
-  insertAt.appendChild(label);
-  insertAt.appendChild(document.createElement("br"));
-  insertAt.appendChild(document.createTextNode(item.subject));
-  insertAt.appendChild(document.createElement("br"));
+  document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
 }
