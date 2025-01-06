f1cb55298024717e50c538c381e1ee3fd4403626
Ning Tang <nintan@microsoft.com>
2025-01-06 16:12:36 +0800

    fix: sample validation rule

--- validation-tool/src/validators/envValidator.ts
+++ validation-tool/src/validators/envValidator.ts
@@ -1,18 +1,20 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import dotenv from "dotenv";
 import fs from "fs-extra";
 import path from "path";
-import dotenv from "dotenv";
 import { Result } from "../resultType";
 
 /**
  * Rule 1: env files shouldn’t contain actual value for the environment variables except for TEAMSFX_ENV and TEAMS_APP_NAME
- * 
+ *
  * @param projectDir root directory of the project
  * @returns validation result
  */
-export default async function validateEnvFiles(projectDir: string): Promise<Result> {
+export default async function validateEnvFiles(
+  projectDir: string
+): Promise<Result> {
   const result: Result = {
     name: "Env Files",
     passed: [],
@@ -22,18 +24,25 @@ export default async function validateEnvFiles(projectDir: string): Promise<Resu
   const files = [".env.dev", ".env.local"];
   for (const envFile of files) {
     const filePath = path.join(projectDir, "env", envFile);
-    if (!await fs.exists(filePath)) {
-      result.failed = [`${path.join("env", envFile)} does not exist.`];
+    if (!(await fs.exists(filePath))) {
+      result.warning = [`${path.join("env", envFile)} does not exist.`];
       continue;
     }
-    const fileContent = await fs.readFile(filePath, 'utf8');
+    const fileContent = await fs.readFile(filePath, "utf8");
     const envData = dotenv.parse(fileContent);
-    const mappings = Object.entries(envData).map(([key, value]) => ({ name: key, value: value }));
+    const mappings = Object.entries(envData).map(([key, value]) => ({
+      name: key,
+      value: value,
+    }));
     let validEnv = true;
     for (const kv of mappings) {
-      if (kv.name === "TEAMSFX_ENV" || kv.name === "APP_NAME_SUFFIX" || kv.name === "TEAMS_APP_NAME") {
+      if (
+        kv.name === "TEAMSFX_ENV" ||
+        kv.name === "APP_NAME_SUFFIX" ||
+        kv.name === "TEAMS_APP_NAME"
+      ) {
         continue;
-      } else if (kv.value !== '') {
+      } else if (kv.value !== "") {
         result.failed.push(`${envFile}: ${kv.name} should NOT have value.`);
         validEnv = false;
       }
