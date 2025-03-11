aea0b77e71081a52b6f00516164b28da0191dad2
Chaoyi Yuan <chyuan@microsoft.com>
2025-03-11 16:51:55 +0800

    feat: initial draft of genai gateway sample

---
+++ genai-gateway/src/bot.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import json
+import traceback
+from dataclasses import asdict
+
+from botbuilder.core import MemoryStorage, TurnContext
+from teams import Application, ApplicationOptions, TeamsAdapter
+from teams.ai import AIOptions
+from teams.ai.models import AzureOpenAIModelOptions, OpenAIModel, OpenAIModelOptions
+from teams.ai.planners import ActionPlanner, ActionPlannerOptions
+from teams.ai.prompts import PromptManager, PromptManagerOptions
+from teams.state import TurnState
+from teams.feedback_loop_data import FeedbackLoopData
+
+from config import Config
+
+config = Config()
+
+# Create AI components
+model: OpenAIModel
+
+model = OpenAIModel(
+    AzureOpenAIModelOptions(
+        api_key=config.AZURE_OPENAI_API_KEY,
+        default_model=config.AZURE_OPENAI_MODEL_DEPLOYMENT_NAME,
+        endpoint=config.AZURE_OPENAI_ENDPOINT,
+    )
+)
+    
+prompts = PromptManager(PromptManagerOptions(prompts_folder=f"{os.getcwd()}/prompts"))
+
+planner = ActionPlanner(
+    ActionPlannerOptions(model=model, prompts=prompts, default_prompt="chat")
+)
+
+# Define storage and application
+storage = MemoryStorage()
+bot_app = Application[TurnState](
+    ApplicationOptions(
+        bot_app_id=config.APP_ID,
+        storage=storage,
+        adapter=TeamsAdapter(config),
+        ai=AIOptions(planner=planner, enable_feedback_loop=True),
+    )
+)
+
+@bot_app.error
+async def on_error(context: TurnContext, error: Exception):
+    # This check writes out errors to console log .vs. app insights.
+    # NOTE: In production environment, you should consider logging this to Azure
+    #       application insights.
+    print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
+    traceback.print_exc()
+
+    # Send a message to the user
+    await context.send_activity("The bot encountered an error or bug.")
+
+@bot_app.feedback_loop()
+async def feedback_loop(_context: TurnContext, _state: TurnState, feedback_loop_data: FeedbackLoopData):
+    # Add custom feedback process logic here.
+    print(f"Your feedback is:\n{json.dumps(asdict(feedback_loop_data), indent=4)}")
